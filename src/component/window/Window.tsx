fdc72c74464c733491d13de948b77f4a2c62e596
TCG-PC\TheCornishGinger <wrigscraft@gmail.com>
2021-10-25 01:43:59 +0100

    Added resizing, animations, style adjustments and prevented dragging out of window.

--- src/component/window/Window.tsx
+++ src/component/window/Window.tsx
@@ -1,35 +1,48 @@
-import { useEffect, useState } from "react";
+import { PureComponent, useEffect, useState } from "react";
 import "./Window.scss";
 
 export function Window() {
-  let videoList = ["izGwDsrQ1eQ", "dQw4w9WgXcQ", "y6120QOlsfU", "L_jWHffIx5E", "k85mRPqvMbE", "ZcJjMnHoIBI"];
+  let videoList = [
+    "izGwDsrQ1eQ",
+    "dQw4w9WgXcQ",
+    "y6120QOlsfU",
+    "L_jWHffIx5E",
+    "k85mRPqvMbE",
+  ];
   let [randomVideo, setRandomVideo] = useState<string>();
 
-  let [controlBarSize, setControlBarSize] = useState<number>(22);
+  let [controlBarSize, setControlBarSize] = useState<number>(30);
   let [controlBarBorder, setControlBarBorder] = useState<number>(0);
   let [windowDrag, setWindowDrag] = useState<boolean>(false);
   let [windowResize, setWindowResize] = useState<boolean>(false);
+  let [windowResizeState, setWindowResizeState] = useState<
+    "bottom" | "left" | "right"
+  >();
   let [windowFullscreen, setWindowFullscreen] = useState<boolean>(false);
   let [windowSize, setWindowSize] = useState<number[]>([
-    window.innerWidth / 2,
-    window.innerHeight / 2,
+    Math.floor(window.innerWidth / 2),
+    Math.floor(window.innerHeight / 2),
   ]);
+  let [bodyHeight, setBodyHeight] = useState<number>();
   let [windowPos, setWindowPos] = useState<number[]>([
     window.innerWidth / 2 - windowSize[0] / 2,
     window.innerHeight / 2 - windowSize[1] / 2,
   ]);
+  let [windowContentSize, setWindowContentSize] = useState<number[]>([0, 0]);
   let [oldWindowSize, setOldWindowSize] = useState<number[]>([0, 0]);
   let [oldWindowPos, setOldWindowPos] = useState<number[]>([0, 0]);
   let [cursorIcon, setCursorIcon] = useState<string>("default");
-  let [windowDisplay, setWindowDisplay] = useState<string>("block");
-  let [windowBodyDisplay, setWindowBodyDisplay] = useState<string>("block");
-  let [windowContentDisplay, setWindowContentDisplay] = useState<string>("block");
-  let [closeIconDisplay, setCloseIconDisplay] = useState<string>("none");
-  let [minIconDisplay, setMinIconDisplay] = useState<string>("none");
-  let [maxIconDisplay, setMaxIconDisplay] = useState<string>("none");
+  let [windowDisplay, setWindowDisplay] = useState<"block" | "none">("block");
+  let [windowBodyDisplay, setWindowBodyDisplay] = useState<"flex" | "none">("flex");
+  let [windowContentInteraction, setWindowContentInteraction] = useState<
+    "all" | "none"
+  >();
+  let [closeIconDisplay, setCloseIconDisplay] = useState<"block" | "none">("none");
+  let [minIconDisplay, setMinIconDisplay] = useState<"block" | "none">("none");
+  let [maxIconDisplay, setMaxIconDisplay] = useState<"block" | "none">("none");
   let [windowTitle, setWindowTitle] = useState<string>("Window");
   let [windowDragOffset, setWindowDragOffset] = useState<number[]>([0, 0]);
-  let [maxBtnText, setMaxBtnText] = useState<string>("<>");
+  let [maxBtnText, setMaxBtnText] = useState<"<>" | "><">("<>");
 
   // ANIMATION
   let btnAnimTime = 150;
@@ -39,44 +52,109 @@ export function Window() {
   let [minBtnTextAnim, setMinBtnTextAnim] = useState<string>();
   let [maxBtnAnim, setMaxBtnAnim] = useState<string>();
   let [maxBtnTextAnim, setMaxBtnTextAnim] = useState<string>();
+  let [windowAnim, setWindowAnim] = useState<"none" | "maxWindow">();
+  let [windowBodyAnim, setWindowBodyAnim] = useState<"none" | "hideWindow">();
 
+  // UPDATE BODY HEIGHT
+  useEffect(() => {
+    setBodyHeight(windowSize[1] - controlBarSize);
+  }, [windowSize, controlBarSize]);
+
+  // MOUSE MOVE
   useEffect(() => {
     let mouseMove = (e: MouseEvent) => {
       if (windowDrag) {
-        setCursorIcon("grabbing");
-        setWindowPos([e.pageX - windowDragOffset[0], e.pageY - windowDragOffset[1]]);
+        let posX = e.x - windowDragOffset[0];
+        let posY = e.y - windowDragOffset[1];
+        if (e.x + windowSize[0] - windowDragOffset[0] > window.innerWidth) {
+          posX = window.innerWidth - windowSize[0];
+        } else if (posX < 2) {
+          posX = 1;
+        }
+        if (e.y + windowSize[1] - windowDragOffset[1] > window.innerHeight) {
+          posY = window.innerHeight - windowSize[1];
+        } else if (posY < 2) {
+          posY = 1;
+        }
+        setWindowPos([posX, posY]);
+      } else if (windowResize) {
+        if (windowResizeState === "bottom") {
+          setWindowSize([windowSize[0], e.y - windowPos[1]]);
+        } else if (windowResizeState === "left") {
+          setWindowPos([e.x, windowPos[1]]);
+          //setWindowSize([windowSize[0], windowSize[1]]);
+        } else if (windowResizeState === "right") {
+          setWindowSize([e.x - windowPos[1], windowSize[1]]);
+        }
       }
     };
     document.addEventListener("mousemove", mouseMove);
     return () => {
       document.removeEventListener("mousemove", mouseMove);
     };
-  }, [windowDrag, windowDragOffset]);
+  }, [
+    windowPos,
+    windowSize,
+    windowDrag,
+    windowResize,
+    windowResizeState,
+    windowDragOffset,
+  ]);
 
+  // WINDOW DRAG OFFSET
   useEffect(() => {
     let mouseDown = (e: MouseEvent) => {
-      if (!windowFullscreen) {
-        setWindowDragOffset([e.x - windowPos[0], e.y - windowPos[1]]);
-      }
+      setWindowDragOffset([
+        Math.floor(e.x) - windowPos[0],
+        Math.floor(e.y) - windowPos[1],
+      ]);
     };
     document.addEventListener("mousedown", mouseDown);
     return () => {
       document.removeEventListener("mousedown", mouseDown);
     };
-  }, [windowPos, windowFullscreen]);
+  }, [windowPos]);
 
+  // WINDOW RESIZE
+  useEffect(() => {
+    if (!windowResize) {
+      setOldWindowSize(windowSize);
+      setWindowContentSize(windowSize);
+    }
+  }, [windowResize]);
+
+  // WINDOW DRAG
+  useEffect(() => {
+    if (windowDrag) {
+    }
+  }, [windowDrag]);
+
+  // MOUSE UP
   useEffect(() => {
     let mouseUp = () => {
-      setCursorIcon("default");
-      setWindowDrag(false);
-      setWindowResize(false);
-      setWindowContentDisplay("block");
+      if (windowResize) {
+        setWindowResize(false);
+      }
+      if (windowDrag) {
+        setWindowDrag(false);
+        setCursorIcon("default");
+      }
     };
     document.addEventListener("mouseup", mouseUp);
     return () => {
       document.removeEventListener("mouseup", mouseUp);
     };
-  }, [windowDrag]);
+  }, [windowResize, windowDrag]);
+
+  // BODY CONTENT INTERACTION
+  useEffect(() => {
+    if (!windowDrag || !windowResize) {
+      setWindowContentInteraction("all");
+    }
+    if (windowDrag || windowResize) {
+      setWindowContentInteraction("none");
+    }
+  }, [windowResize, windowDrag]);
 
   let enableDrag = () => {
     if (!windowResize) {
@@ -84,19 +162,34 @@ export function Window() {
         setWindowSize(oldWindowSize);
         setWindowFullscreen(false);
       }
-      setWindowContentDisplay("none");
       setCursorIcon("grabbing");
       setWindowDrag(true);
     }
   };
 
-  let enableResizeH = () => {
-    setWindowResize(true);
+  // ENABLE WINDOW RESIZE
+  let enableResizeBottom = () => {
+    if (controlBarBorder === 0) {
+      setWindowResizeState("bottom");
+      setWindowResize(true);
+    }
+  };
+  let enableResizeLeft = () => {
+    if (controlBarBorder === 0) {
+      setWindowResizeState("left");
+      setWindowResize(true);
+    }
+  };
+  let enableResizeRight = () => {
+    if (controlBarBorder === 0) {
+      setWindowResizeState("right");
+      setWindowResize(true);
+    }
   };
 
   // CLOSE BUTTON
   let closeWindow = () => {
-    if (!windowDrag) {
+    if (!windowDrag && !windowResize) {
       setWindowDisplay("none");
     }
   };
@@ -115,12 +208,13 @@ export function Window() {
 
   // MIN BUTTON
   let minWindow = () => {
-    if (!windowDrag) {
-      if (windowBodyDisplay === "none") {
-        setWindowBodyDisplay("block");
+    if (!windowDrag && !windowResize) {
+      if (controlBarBorder > 0) {
+        setWindowBodyDisplay("flex");
+        setWindowBodyAnim("none");
         setControlBarBorder(0);
       } else {
-        setWindowBodyDisplay("none");
+        setWindowBodyAnim("hideWindow");
         setControlBarBorder(5);
       }
     }
@@ -140,17 +234,23 @@ export function Window() {
 
   // MAX BUTTON
   let maxWindow = () => {
-    if (!windowDrag) {
+    if (!windowDrag && !windowResize) {
       if (windowFullscreen) {
         setWindowSize(oldWindowSize);
+        setWindowContentSize(oldWindowSize);
         setWindowPos(oldWindowPos);
         setMaxBtnText("<>");
       } else {
         setOldWindowPos(windowPos);
-        setWindowPos([0, 0]);
-        setOldWindowSize(windowSize);
-        setWindowSize([window.innerWidth, window.innerHeight]);
-        setMaxBtnText("><");
+        setWindowAnim("maxWindow");
+        setTimeout(() => {
+          setWindowPos([0, 0]);
+          setOldWindowSize(windowSize);
+          setWindowSize([window.innerWidth, window.innerHeight]);
+          setWindowContentSize([window.innerWidth, window.innerHeight]);
+          setMaxBtnText("><");
+          setWindowAnim("none");
+        }, 400);
       }
       setWindowFullscreen(!windowFullscreen);
     }
@@ -179,9 +279,11 @@ export function Window() {
     setCursorIcon("default");
   };
 
+  // INIT
   let windowInit = () => {
     setWindowTitle(windowTitle);
-    setControlBarSize(22);
+    setControlBarSize(30);
+    setOldWindowSize(windowSize);
   };
 
   let getRandomVideo = () => {
@@ -192,9 +294,10 @@ export function Window() {
 
   return (
     <div
-      className="window"
+      className="window no-select"
       onLoad={windowInit}
       style={{
+        animationName: windowAnim,
         display: windowDisplay,
         top: windowPos[1],
         left: windowPos[0],
@@ -250,32 +353,37 @@ export function Window() {
             </p>
           </div>
         </div>
-        <div className="window-drag-area" onPointerDown={enableDrag}>
-          <div>
-            <p className="window-title">{windowTitle}</p>
-          </div>
+        <div className="window-drag-area" onPointerDown={enableDrag}></div>
+        <div className="window-title-wrap" onPointerDown={enableDrag}>
+          <p className="window-title">{windowTitle}</p>
         </div>
       </div>
       <div
         className="window-body"
         style={{
-          height: windowSize[1] - controlBarSize,
+          animationName: windowBodyAnim,
+          width: windowSize[0],
+          height: bodyHeight,
           display: windowBodyDisplay,
         }}
       >
         <div
           className="resize-box left"
+          style={{ height: bodyHeight }}
+          onMouseDown={enableResizeLeft}
           onMouseEnter={setCursorResizeW}
           onMouseLeave={setCursorNormal}
         ></div>
         <div
           className="resize-box bottom"
-          onMouseDown={enableResizeH}
+          onMouseDown={enableResizeBottom}
           onMouseEnter={setCursorResizeH}
           onMouseLeave={setCursorNormal}
         ></div>
         <div
           className="resize-box right"
+          style={{ height: bodyHeight }}
+          onMouseDown={enableResizeRight}
           onMouseEnter={setCursorResizeW}
           onMouseLeave={setCursorNormal}
         ></div>
@@ -285,7 +393,11 @@ export function Window() {
           allowFullScreen={false}
           className="window-content"
           src={"https://www.youtube.com/embed/" + randomVideo + "?autoplay=1"}
-          style={{ display: windowContentDisplay }}
+          style={{
+            width: windowContentSize[0] - 2,
+            height: windowContentSize[1] - controlBarSize - 1,
+            pointerEvents: windowContentInteraction,
+          }}
         ></iframe>
       </div>
     </div>

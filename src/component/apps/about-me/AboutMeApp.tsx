200541acaba70c6d7bdb40f7f0b17e69093310f6
TCG-PC\TheCornishGinger <wrigscraft@gmail.com>
2021-11-14 13:20:56 +0000

    Update about me story.

--- src/component/apps/about-me/AboutMeApp.tsx
+++ src/component/apps/about-me/AboutMeApp.tsx
@@ -20,38 +20,43 @@ export function AboutMeApp() {
               </span>
             </div>
             <span>
-              It's mid October 2020, my best friend's just married so I have more
-              time on my hands. Shortly before the wedding I had once again taken up
-              game design for the 1000th time, and started work making a 2D horror
-              game using the defold game engine.
+              It's mid October 2020, my best friends just married so I have more time
+              on my hands. Shortly before the wedding I had once again taken up game
+              design for the 1000th time, and started work on a 2D horror game using
+              the defold game engine.
               <div className="gap" />
               After spending a while getting the bare bones working, I began to
-              struggle with implementing features I really wanted. After getting
-              advice from a friend I realised it's time to switch over to something
-              with more tutorials (so I can get more help).
+              struggle with implementing features I really wanted. I got some advice
+              and decided it was time to switch over to something with more tutorials
+              and support.
               <div className="gap" />
-              The thought of starting all over again was too much to handle at the
-              time so I turned my attention to my website, I wanted a cool place to
-              host the games. My old website hadn't been touched in a long time,
-              needless to say it didn't look great.
+              The thought of starting all over again was a bit much at the time, so I
+              turned my attention to a website. I wanted a cool place to host the
+              games, but my old site hadn't been touched in a long time and needless
+              to say it didn't look great.
               <div className="gap" />
-              Time for a makeover! I poured a couple hours into it (because I'm
-              particular), and was super pleased with the results.
+              Time for a makeover! I poured hours into adjusting each bit until I was
+              happy with it, and was super pleased with the results.
               <div className="gap" />
-              BUT THEN, being the excited man I was, I sent it to my friend only to
-              get LAUGHED AT BECAUSE IT HAD A WIX BANNER ON IT. *cries in drag and
-              drop*
+              But then, being the excited man I was, I sent it to my friend only to
+              get LAUGHED AT because it had.. a wix banner on it... *cries in drag
+              and drop*
               <div className="gap" />
-              It suddenly dawned on me, that if I couldn't code a website HOW WOULD I
-              CODE A WHOLE GAME...
+              It suddenly dawned on me, if I couldn't code a website, how would i
+              code a freaking game...
               <div className="gap" />
-              And with that I got to work... And then had to restart, because
-              reasons...
+              With that I got to work. After one restart, and then another, and some
+              bugs here and there we are finally here!
               <div className="gap" />
-              Anddd after more resarts (and a lot of help) we reach this website,
-              version 3! iTs sO mUcH cOoLeR!!!
+              So now you wasted your time on my story I'll reward you with some
+              advice. If you have a goal, like making a website, don't give even even
+              if you have to start all over again! There are many great discord
+              servers and stack overflow pages with information ready for your
+              taking!
             </span>
-            <p className="text small">Created by Levi Wrigley.</p>
+            <p className="text small">
+              Thank you for reading, website by Levi Wrigley.
+            </p>
           </div>
         </div>
       </div>
